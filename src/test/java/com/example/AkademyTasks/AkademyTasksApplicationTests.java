164d545f2cd731005bb32650e499bd5b08ab2910
Fokusmod <Gang32rus@mail.ru>
2025-05-02 13:40:07 +0300

    Initial commit

---
+++ src/test/java/com/example/AkademyTasks/AkademyTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AkademyTasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkademyTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
